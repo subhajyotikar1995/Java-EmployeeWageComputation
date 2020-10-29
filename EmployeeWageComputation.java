4640ca94a66575a586d57c4c52603911117f4f97
subhajyoti <subhajyotikar0@gmail.com>
2020-10-29 11:02:16 +0530

    employee is present or absent

---
+++ EmployeeWageComputation.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+public class EmpWageComp {
+    public static void main(String[] args){
+        Random ran= new Random();
+        int ran1 = ran.nextInt(2);
+
+        if( ran1 == 0 ) {
+            System.out.println("Employee is Present");
+        }else {
+            System.out.println("Employee is Present");
+        }
+    }
+}
