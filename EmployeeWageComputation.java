0d5737ab2c29137c7e7082cf8c0edd129dd1f642
subhajyoti <subhajyotikar0@gmail.com>
2020-10-30 12:11:31 +0530

    using switch case

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,24 +1,29 @@
-import java.util.Random;
-
 public class EmpWageComp {
     public static void main(String[] args){
-        
+       
         EmpWageComp ewc = new EmpWageComp();
 
+    
         Random ran= new Random();
         int ran1 = ran.nextInt(3);
 
-        if( ran1 == 0 ) {
-            System.out.println("Employee is Present and Working FullTime");
-            ewc.fullTime();
-        }else if (ran1 == 1){
-            System.out.println("Employee is Present and Working PartTime");
-            ewc.partTime();
-        }else {
-            System.out.println("Employee is Absent");
+        
+        switch (ran1){
+            case 0 :
+                System.out.println("Employee is Present and Working FullTime");
+                ewc.fullTime();
+                break;
+            case 1:
+                System.out.println("Employee is Present and Working PartTime");
+                ewc.partTime();
+                break;
+            case 2:
+                System.out.println("Employee is Absent");
+                break;
         }
 
     }
+    
     public void fullTime(){
         int wagePerHr = 20;
         int dailyWorkHrs = 8;
@@ -26,6 +31,7 @@ public class EmpWageComp {
         int totalDayWage = wagePerHr * dailyWorkHrs;
         System.out.println("Wage : " + totalDayWage);
     }
+ 
     public void partTime(){
         int wagePerHr = 20;
         int dailyWorkHrs = 4;
@@ -34,4 +40,3 @@ public class EmpWageComp {
         System.out.println("Wage : " + totalDayWage);
     }
 }
-
