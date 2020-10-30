fb74eeba75d33fafef82e1ce908342b28fd3a41b
subhajyoti <subhajyotikar0@gmail.com>
2020-10-30 12:22:09 +0530

    calculate monthly wage

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,13 +1,15 @@
+import java.util.Random;
+
 public class EmpWageComp {
     public static void main(String[] args){
        
         EmpWageComp ewc = new EmpWageComp();
 
-    
+     
         Random ran= new Random();
         int ran1 = ran.nextInt(3);
 
-        
+     
         switch (ran1){
             case 0 :
                 System.out.println("Employee is Present and Working FullTime");
@@ -23,20 +25,24 @@ public class EmpWageComp {
         }
 
     }
-    
+  
     public void fullTime(){
         int wagePerHr = 20;
         int dailyWorkHrs = 8;
+        int workingDays = 20;
 
-        int totalDayWage = wagePerHr * dailyWorkHrs;
-        System.out.println("Wage : " + totalDayWage);
+        int totalMonthWage = wagePerHr * dailyWorkHrs * workingDays;
+        System.out.println("Monthly Wage : " + totalMonthWage);
     }
- 
+    
     public void partTime(){
         int wagePerHr = 20;
         int dailyWorkHrs = 4;
+        int workingDays = 20;
 
-        int totalDayWage = wagePerHr * dailyWorkHrs;
-        System.out.println("Wage : " + totalDayWage);
+        int totalMonthWage = wagePerHr * dailyWorkHrs * workingDays;
+        System.out.println("Monthly Wage : " + totalMonthWage);
     }
 }
+
+
